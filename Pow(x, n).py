435a1ea6e4bba8a66ec50a95829f4adb558c6c44
mustahidhasan19 <mustahidhasan9@gmail.com>
2022-11-21 01:15:17 +0600

    commit 1

---
+++ Pow(x, n).py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        result = float(x) ** int(n)
+        return result
